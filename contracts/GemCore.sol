816c38cb70a37825dd3859d83cc17c95c97ab696
srahim6 <srahim6@gatech.edu>
2018-06-02 03:07:30 -0400

    Return gemId from getGem

--- contracts/GemCore.sol
+++ contracts/GemCore.sol
@@ -396,12 +396,14 @@ contract GemCore is GemMining {
         uint256 morphTime,
         uint256 price,
         uint256 rarity,
+        uint256 id,
         address owner
     ) {
         Gem storage myGem = gems[_id];
         morphTime = uint256(myGem.morphTime);
         rarity = myGem.rarity;
         price = myGem.price;
+        id = _id;
         owner = gemIndexToOwner[_id];
     }
 }
