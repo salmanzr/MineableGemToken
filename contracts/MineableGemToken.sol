b2c67299aa9e7df0de54b5588dab93ce4cf4fec8
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2018-06-01 21:31:26 -0400

    Continued editing the mint function, added per-user mappings

--- contracts/MineableGemToken.sol
+++ contracts/MineableGemToken.sol
@@ -218,32 +218,20 @@ contract MineableGemToken is ERC721Interface, Owned {
     uint public  _MAXIMUM_TARGET = 2**234;
 
 		// The actual difficulty to mine at
-		// @TODO port this over to a per-user mapping
-    uint public miningTarget;
+		// Different per user
+    mapping(address => uint) public miningTarget;
 
 		// Challenge number - means that no one can submit the same hash twice
-		// @TODO port this over to a per-user mapping
-		// A new one is generated whenever tokens are minted
-    bytes32 public challengeNumber;
-
-		// @TODO for now, remove these - they aren't useful for ERC-721
-		// (especially with variable difficulty)
-    uint public rewardEra;
-    uint public maxSupplyForEra;
+		// A new one is generated for a user when that user mints as new gem
+    mapping(address => bytes32) public challengeNumber;
 
 		// Just stores the last person that created a MGT
     address public lastRewardTo;
 
-		// @TODO remove this, because we're only minting 1 token each time
-    uint public lastRewardAmount;
-
 		// Just stores the last block number a token was created
     uint public lastRewardEthBlockNumber;
 
-	  // Prevents someone from calling the constructor twice
-		// @TODO remove this
-    bool locked = false;
-
+		// Stores the solutions for each challenge so that people can't submit a solution twice
     mapping(bytes32 => bytes32) solutionForChallenge;
 
     uint public tokensMinted;
@@ -318,11 +306,6 @@ contract MineableGemToken is ERC721Interface, Owned {
     // Verify that the provided hash is leq than the mining target
     if(uint256(digest) > miningTarget) revert();
 
-    // Only allow one reward for each challenge
-    bytes32 solution = solutionForChallenge[challengeNumber];
-    solutionForChallenge[challengeNumber] = digest;
-    if(solution != 0x0) revert();  //prevent the same answer from awarding twice
-
 		// @TODO here is where we want to actually mint the coins
 		mintGemToken(miningTarget);
     tokensMinted = tokensMinted.add(1);
@@ -331,330 +314,204 @@ contract MineableGemToken is ERC721Interface, Owned {
     lastRewardTo = msg.sender;
     lastRewardEthBlockNumber = block.number;
 
-		// TODO set new challenge number
+		// Set new challenge number for the user
+		// This is just the hash of the nonce, which will change every time
+		// (because we never have an identical hash)
+		challengeNumber[msg.sender] = keccak256(nonce)
 
-		Mint(msg.sender, challengeNumber );
+		Mint(msg.sender, challengeNumber[msg.sender]);
+
+    return true;
+  }
+
+	// Get the user's challenge number
+  function getChallengeNumber() public constant returns (bytes32) {
+    return challengeNumber[msg.sender];
+  }
+
+  // Gets the number of zeroes the digest of the PoW solution requires (different per user)
+  function getMiningDifficulty() public constant returns (uint) {
+    return _MAXIMUM_TARGET.div(miningTarget[msg.sender]);
+  }
+
+	// Gets a user's mining target
+  function getMiningTarget() public constant returns (uint) {
+    return miningTarget[msg.sender];
+  }
+
+  // Helps debug mining software
+  function getMintDigest(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number) public view returns (bytes32 digesttest) {
+
+    bytes32 digest = keccak256(challenge_number[msg.sender], msg.sender, nonce);
+
+    return digest;
+  }
+
+  // Helps debug mining software
+  function checkMintSolution(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number, uint testTarget) public view returns (bool success) {
+
+    bytes32 digest = keccak256(challenge_number, msg.sender, nonce);
+
+    if(uint256(digest) > testTarget) revert();
+
+    return (digest == challenge_digest);
+  }
+
+  // ------------------------------------------------------------------------
+
+  // Total supply
+
+  // ------------------------------------------------------------------------
+
+  function totalSupply() public constant returns (uint) {
+
+    return _totalSupply  - balances[address(0)];
+  }
+
+  // ------------------------------------------------------------------------
+
+  // Get the token balance for account `tokenOwner`
+
+  // ------------------------------------------------------------------------
+
+  function balanceOf(address tokenOwner) public constant returns (uint balance) {
+
+    return balances[tokenOwner];
+  }
+
+  // ------------------------------------------------------------------------
+
+  // Transfer the balance from token owner's account to `to` account
+
+  // - Owner's account must have sufficient balance to transfer
+
+  // - 0 value transfers are allowed
+
+  // ------------------------------------------------------------------------
+
+  function transfer(address to, uint tokens) public returns (bool success) {
+
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+
+    balances[to] = balances[to].add(tokens);
+
+    Transfer(msg.sender, to, tokens);
 
     return true;
 
   }
 
+  // ------------------------------------------------------------------------
 
-    function _startNewMiningEpoch() internal {
+  // Token owner can approve for `spender` to transferFrom(...) `tokens`
 
-      //if max supply for the era will be exceeded next reward round then enter the new era before that happens
+  // from the token owner's account
 
-      //40 is the final reward era, almost all tokens minted
-      //once the final era is reached, more tokens will not be given out because the assert function
-      if( tokensMinted.add(getMiningReward()) > maxSupplyForEra && rewardEra < 39)
-      {
-        rewardEra = rewardEra + 1;
-      }
+  //
 
-      //set the next minted supply at which the era will change
-      // total supply is 2100000000000000  because of 8 decimal places
-      maxSupplyForEra = _totalSupply - _totalSupply.div( 2**(rewardEra + 1));
+  // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
 
-      epochCount = epochCount.add(1);
+  // recommends that there are no checks for the approval double-spend attack
 
-      //every so often, readjust difficulty. Dont readjust when deploying
-      if(epochCount % _BLOCKS_PER_READJUSTMENT == 0)
-      {
-        _reAdjustDifficulty();
-      }
+  // as this should be implemented in user interfaces
 
+  // ------------------------------------------------------------------------
 
-      //make the latest ethereum block hash a part of the next challenge for PoW to prevent pre-mining future blocks
-      //do this last since this is a protection mechanism in the mint() function
-      challengeNumber = block.blockhash(block.number - 1);
+  function approve(address spender, uint tokens) public returns (bool success) {
 
+    allowed[msg.sender][spender] = tokens;
 
+    Approval(msg.sender, spender, tokens);
 
+    return true;
 
+  }
 
+  // ------------------------------------------------------------------------
 
-    }
+  // Transfer `tokens` from the `from` account to the `to` account
 
+  //
 
+  // The calling account must already have sufficient tokens approve(...)-d
 
+  // for spending from the `from` account and
 
-    //https://en.bitcoin.it/wiki/Difficulty#What_is_the_formula_for_difficulty.3F
-    //as of 2017 the bitcoin difficulty was up to 17 zeroes, it was only 8 in the early days
+  // - From account must have sufficient balance to transfer
 
-    //readjust the target by 5 percent
-    function _reAdjustDifficulty() internal {
+  // - Spender must have sufficient allowance to transfer
 
+  // - 0 value transfers are allowed
 
-        uint ethBlocksSinceLastDifficultyPeriod = block.number - latestDifficultyPeriodStarted;
-        //assume 360 ethereum blocks per hour
+  // ------------------------------------------------------------------------
 
-        //we want miners to spend 10 minutes to mine each 'block', about 60 ethereum blocks = one 0xbitcoin epoch
-        uint epochsMined = _BLOCKS_PER_READJUSTMENT; //256
+  function transferFrom(address from, address to, uint tokens) public returns (bool success) {
 
-        uint targetEthBlocksPerDiffPeriod = epochsMined * 60; //should be 60 times slower than ethereum
+    balances[from] = balances[from].sub(tokens);
 
-        //if there were less eth blocks passed in time than expected
-        if( ethBlocksSinceLastDifficultyPeriod < targetEthBlocksPerDiffPeriod )
-        {
-          uint excess_block_pct = (targetEthBlocksPerDiffPeriod.mul(100)).div( ethBlocksSinceLastDifficultyPeriod );
+    allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
 
-          uint excess_block_pct_extra = excess_block_pct.sub(100).limitLessThan(1000);
-          // If there were 5% more blocks mined than expected then this is 5.  If there were 100% more blocks mined than expected then this is 100.
+    balances[to] = balances[to].add(tokens);
 
-          //make it harder
-          miningTarget = miningTarget.sub(miningTarget.div(2000).mul(excess_block_pct_extra));   //by up to 50 %
-        }else{
-          uint shortage_block_pct = (ethBlocksSinceLastDifficultyPeriod.mul(100)).div( targetEthBlocksPerDiffPeriod );
+    Transfer(from, to, tokens);
 
-          uint shortage_block_pct_extra = shortage_block_pct.sub(100).limitLessThan(1000); //always between 0 and 1000
+    return true;
+  }
 
-          //make it easier
-          miningTarget = miningTarget.add(miningTarget.div(2000).mul(shortage_block_pct_extra));   //by up to 50 %
-        }
+  // ------------------------------------------------------------------------
 
+  // Returns the amount of tokens approved by the owner that can be
 
+  // transferred to the spender's account
 
-        latestDifficultyPeriodStarted = block.number;
+  // ------------------------------------------------------------------------
 
-        if(miningTarget < _MINIMUM_TARGET) //very difficult
-        {
-          miningTarget = _MINIMUM_TARGET;
-        }
+  function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
 
-        if(miningTarget > _MAXIMUM_TARGET) //very easy
-        {
-          miningTarget = _MAXIMUM_TARGET;
-        }
-    }
+    return allowed[tokenOwner][spender];
+  }
 
+  // ------------------------------------------------------------------------
 
-    //this is a recent ethereum block hash, used to prevent pre-mining future blocks
-    function getChallengeNumber() public constant returns (bytes32) {
-        return challengeNumber;
-    }
+  // Token owner can approve for `spender` to transferFrom(...) `tokens`
 
-    //the number of zeroes the digest of the PoW solution requires.  Auto adjusts
-     function getMiningDifficulty() public constant returns (uint) {
-        return _MAXIMUM_TARGET.div(miningTarget);
-    }
+  // from the token owner's account. The `spender` contract function
 
-    function getMiningTarget() public constant returns (uint) {
-       return miningTarget;
-   }
+  // `receiveApproval(...)` is then executed
 
+  // ------------------------------------------------------------------------
 
+  function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
 
-    //21m coins total
-    //reward begins at 50 and is cut in half every reward era (as tokens are mined)
-    function getMiningReward() public constant returns (uint) {
-        //once we get half way thru the coins, only get 25 per block
+    allowed[msg.sender][spender] = tokens;
 
-         //every reward era, the reward amount halves.
+    Approval(msg.sender, spender, tokens);
 
-         return (50 * 10**uint(decimals) ).div( 2**rewardEra ) ;
+    ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
 
-    }
+    return true;
+  }
 
-    //help debug mining software
-    function getMintDigest(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number) public view returns (bytes32 digesttest) {
+  // ------------------------------------------------------------------------
 
-        bytes32 digest = keccak256(challenge_number,msg.sender,nonce);
+  // Don't accept ETH
 
-        return digest;
+  // ------------------------------------------------------------------------
 
-      }
+  function () public payable {
 
-        //help debug mining software
-      function checkMintSolution(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number, uint testTarget) public view returns (bool success) {
+    revert();
+  }
 
-          bytes32 digest = keccak256(challenge_number,msg.sender,nonce);
+  // ------------------------------------------------------------------------
 
-          if(uint256(digest) > testTarget) revert();
+  // Owner can transfer out any accidentally sent ERC20 tokens
 
-          return (digest == challenge_digest);
+  // ------------------------------------------------------------------------
 
-        }
+  function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
 
+    return ERC20Interface(tokenAddress).transfer(owner, tokens);
 
-
-    // ------------------------------------------------------------------------
-
-    // Total supply
-
-    // ------------------------------------------------------------------------
-
-    function totalSupply() public constant returns (uint) {
-
-        return _totalSupply  - balances[address(0)];
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Get the token balance for account `tokenOwner`
-
-    // ------------------------------------------------------------------------
-
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-
-        return balances[tokenOwner];
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Transfer the balance from token owner's account to `to` account
-
-    // - Owner's account must have sufficient balance to transfer
-
-    // - 0 value transfers are allowed
-
-    // ------------------------------------------------------------------------
-
-    function transfer(address to, uint tokens) public returns (bool success) {
-
-        balances[msg.sender] = balances[msg.sender].sub(tokens);
-
-        balances[to] = balances[to].add(tokens);
-
-        Transfer(msg.sender, to, tokens);
-
-        return true;
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-
-    // from the token owner's account
-
-    //
-
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-
-    // recommends that there are no checks for the approval double-spend attack
-
-    // as this should be implemented in user interfaces
-
-    // ------------------------------------------------------------------------
-
-    function approve(address spender, uint tokens) public returns (bool success) {
-
-        allowed[msg.sender][spender] = tokens;
-
-        Approval(msg.sender, spender, tokens);
-
-        return true;
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Transfer `tokens` from the `from` account to the `to` account
-
-    //
-
-    // The calling account must already have sufficient tokens approve(...)-d
-
-    // for spending from the `from` account and
-
-    // - From account must have sufficient balance to transfer
-
-    // - Spender must have sufficient allowance to transfer
-
-    // - 0 value transfers are allowed
-
-    // ------------------------------------------------------------------------
-
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-
-        balances[from] = balances[from].sub(tokens);
-
-        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-
-        balances[to] = balances[to].add(tokens);
-
-        Transfer(from, to, tokens);
-
-        return true;
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Returns the amount of tokens approved by the owner that can be
-
-    // transferred to the spender's account
-
-    // ------------------------------------------------------------------------
-
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-
-        return allowed[tokenOwner][spender];
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-
-    // from the token owner's account. The `spender` contract function
-
-    // `receiveApproval(...)` is then executed
-
-    // ------------------------------------------------------------------------
-
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-
-        allowed[msg.sender][spender] = tokens;
-
-        Approval(msg.sender, spender, tokens);
-
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-
-        return true;
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Don't accept ETH
-
-    // ------------------------------------------------------------------------
-
-    function () public payable {
-
-        revert();
-
-    }
-
-
-
-    // ------------------------------------------------------------------------
-
-    // Owner can transfer out any accidentally sent ERC20 tokens
-
-    // ------------------------------------------------------------------------
-
-    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-
-        return ERC20Interface(tokenAddress).transfer(owner, tokens);
-
-    }
-
+  }
 }
